8e425c8e2e23272b8c847e3285c5e0391afd38b0
Ghadi Ghanem <ghadighanem27@gmail.com>
2025-11-03 20:44:59 +0100

    rr

--- lib/signals.py
+++ lib/signals.py
@@ -19,6 +19,7 @@ def signals(score, price, symbol):
     if score >= 30:
         if position != "BUY":
             
+            print(f"🟢 BUY | Price:{price} | score: {score:.2f}")
             message =  ("📊 TRADE SIGNAL\n"
             "-----------------------------\n"
             "Action      |    🟢 BUY\n"
@@ -26,13 +27,13 @@ def signals(score, price, symbol):
             f"Price        |    {price:.2f}\n"
             f"Score       |    {score:.2f}\n"
             "-----------------------------")
-            print(message)
             log_signal("🟢 BUY", score, price)
             send_message_sync(message)
             position = "BUY"
 
     elif score <= 20:
         if position != "SELL":
+            print(f"🔴 SELL | Price:{price} | score: {score:.2f}")
             message =  ("📊 TRADE SIGNAL\n"
             "-----------------------------\n"
             "Action      |    🔴 SELL\n"
@@ -40,11 +41,10 @@ def signals(score, price, symbol):
             f"Price        |    {price:.2f}\n"
             f"Score       |    {score:.2f}\n"
             "-----------------------------")
-            print(message)
             log_signal("🔴 SELL", score, price)
             send_message_sync(message)
             position = "SELL"
 
     else:
-        print(f"⚪ HOLD | Price:{price} | score: {score:.2f}" )
+        print(f"⚪ HOLD | Price:{price} | score: {score:.2f}")
         log_signal("⚪ HOLD", score, price)
